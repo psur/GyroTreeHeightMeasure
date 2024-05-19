c2d0f9b975dc254562be23d5e29917c3a8f86f55
Peter Surovy <psurovy@gmail.com>
2024-05-19 20:50:33 +0200

    Initial Commit

---
+++ GyroTreeHeightMeasure/GyroTreeHeightMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroTreeHeightMeasureApp.swift
+//  GyroTreeHeightMeasure
+//
+//  Created by Peter Surovy on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GyroTreeHeightMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
